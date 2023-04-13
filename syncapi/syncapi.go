130c2d0d5d899bae33e1400329e578aaff8f4716
fedekunz <115453243+fedekunz@users.noreply.github.com>
2023-04-13 16:27:07 +0800

    syncapi

---
+++ syncapi/syncapi.go
@@ -0,0 +1,147 @@
+// Copyright 2017 Vector Creations Ltd
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package syncapi
+
+import (
+	"context"
+
+	"github.com/matrix-org/dendrite/internal/caching"
+	"github.com/sirupsen/logrus"
+
+	keyapi "github.com/matrix-org/dendrite/keyserver/api"
+	"github.com/matrix-org/dendrite/roomserver/api"
+	"github.com/matrix-org/dendrite/setup/base"
+	"github.com/matrix-org/dendrite/setup/jetstream"
+	userapi "github.com/matrix-org/dendrite/userapi/api"
+
+	"github.com/matrix-org/dendrite/syncapi/consumers"
+	"github.com/matrix-org/dendrite/syncapi/notifier"
+	"github.com/matrix-org/dendrite/syncapi/producers"
+	"github.com/matrix-org/dendrite/syncapi/routing"
+	"github.com/matrix-org/dendrite/syncapi/storage"
+	"github.com/matrix-org/dendrite/syncapi/streams"
+	"github.com/matrix-org/dendrite/syncapi/sync"
+)
+
+// AddPublicRoutes sets up and registers HTTP handlers for the SyncAPI
+// component.
+func AddPublicRoutes(
+	base *base.BaseDendrite,
+	userAPI userapi.SyncUserAPI,
+	rsAPI api.SyncRoomserverAPI,
+	keyAPI keyapi.SyncKeyAPI,
+) {
+	cfg := &base.Cfg.SyncAPI
+
+	js, natsClient := base.NATS.Prepare(base.ProcessContext, &cfg.Matrix.JetStream)
+
+	syncDB, err := storage.NewSyncServerDatasource(base, &cfg.Database)
+	if err != nil {
+		logrus.WithError(err).Panicf("failed to connect to sync db")
+	}
+
+	eduCache := caching.NewTypingCache()
+	notifier := notifier.NewNotifier()
+	streams := streams.NewSyncStreamProviders(syncDB, userAPI, rsAPI, keyAPI, eduCache, base.Caches, notifier)
+	notifier.SetCurrentPosition(streams.Latest(context.Background()))
+	if err = notifier.Load(context.Background(), syncDB); err != nil {
+		logrus.WithError(err).Panicf("failed to load notifier ")
+	}
+
+	federationPresenceProducer := &producers.FederationAPIPresenceProducer{
+		Topic:     cfg.Matrix.JetStream.Prefixed(jetstream.OutputPresenceEvent),
+		JetStream: js,
+	}
+	presenceConsumer := consumers.NewPresenceConsumer(
+		base.ProcessContext, cfg, js, natsClient, syncDB,
+		notifier, streams.PresenceStreamProvider,
+		userAPI,
+	)
+
+	requestPool := sync.NewRequestPool(syncDB, cfg, userAPI, keyAPI, rsAPI, streams, notifier, federationPresenceProducer, presenceConsumer, base.EnableMetrics)
+
+	if err = presenceConsumer.Start(); err != nil {
+		logrus.WithError(err).Panicf("failed to start presence consumer")
+	}
+
+	userAPIStreamEventProducer := &producers.UserAPIStreamEventProducer{
+		JetStream: js,
+		Topic:     cfg.Matrix.JetStream.Prefixed(jetstream.OutputStreamEvent),
+	}
+
+	userAPIReadUpdateProducer := &producers.UserAPIReadProducer{
+		JetStream: js,
+		Topic:     cfg.Matrix.JetStream.Prefixed(jetstream.OutputReadUpdate),
+	}
+
+	keyChangeConsumer := consumers.NewOutputKeyChangeEventConsumer(
+		base.ProcessContext, cfg, cfg.Matrix.JetStream.Prefixed(jetstream.OutputKeyChangeEvent), 
+		js, rsAPI, syncDB, notifier,
+		streams.DeviceListStreamProvider,
+	)
+	if err = keyChangeConsumer.Start(); err != nil {
+		logrus.WithError(err).Panicf("failed to start key change consumer")
+	}
+
+	roomConsumer := consumers.NewOutputRoomEventConsumer( 
+		base.ProcessContext, cfg, js, syncDB, notifier, streams.PDUStreamProvider,
+		streams.InviteStreamProvider, rsAPI, userAPIStreamEventProducer,
+	)
+	if err = roomConsumer.Start(); err != nil {
+		logrus.WithError(err).Panicf("failed to start room server consumer")
+	}
+
+	clientConsumer := consumers.NewOutputClientDataConsumer(
+		base.ProcessContext, cfg, js, syncDB, notifier, streams.AccountDataStreamProvider,
+		userAPIReadUpdateProducer,
+	)
+	if err = clientConsumer.Start(); err != nil {
+		logrus.WithError(err).Panicf("failed to start client data consumer")
+	}
+
+	notificationConsumer := consumers.NewOutputNotificationDataConsumer(
+		base.ProcessContext, cfg, js, syncDB, notifier, streams.NotificationDataStreamProvider,
+	)
+	if err = notificationConsumer.Start(); err != nil {
+		logrus.WithError(err).Panicf("failed to start notification data consumer")
+	}
+
+	typingConsumer := consumers.NewOutputTypingEventConsumer(
+		base.ProcessContext, cfg, js, eduCache, notifier, streams.TypingStreamProvider,
+	)
+	if err = typingConsumer.Start(); err != nil {
+		logrus.WithError(err).Panicf("failed to start typing consumer")
+	}
+
+	sendToDeviceConsumer := consumers.NewOutputSendToDeviceEventConsumer(
+		base.ProcessContext, cfg, js, syncDB, notifier, streams.SendToDeviceStreamProvider,
+	)
+	if err = sendToDeviceConsumer.Start(); err != nil {
+		logrus.WithError(err).Panicf("failed to start send-to-device consumer")
+	}
+
+	receiptConsumer := consumers.NewOutputReceiptEventConsumer(
+		base.ProcessContext, cfg, js, syncDB, notifier, streams.ReceiptStreamProvider,
+		userAPIReadUpdateProducer,
+	)
+	if err = receiptConsumer.Start(); err != nil {
+		logrus.WithError(err).Panicf("failed to start receipts consumer")
+	}
+
+	routing.Setup(
+		base.PublicClientAPIMux, requestPool, syncDB, userAPI,
+		rsAPI, cfg, base.Caches,
+	)
+}
