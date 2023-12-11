70632551139697fbfae8e5f438d69495da8cca15
Gervais Brochard <115452844+pamsquid@users.noreply.github.com>
2023-12-11 18:09:29 +0800

    group limit

---
+++ new_feature/group-limit/init.go
@@ -0,0 +1,10 @@
+package group_limit
+
+// 1，account_groups  warn 
+//：
+//：
+//{"visibility":"public","room_alias_name":"4455","name":"2","topic":"","preset":"public_chat"}
+
+// 2，account_groups （）
+// （syncapi_current_room_stateIDm.room.create）
+// 3， （，state，）APP，
